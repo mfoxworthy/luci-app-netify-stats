014885aabb5990fbca594480874005ef2066efc6
Mike Foxworthy <mike.foxworthy@viasat.com>
2026-06-01 10:01:46 -0700

    fix(view): move chart.js to plain resource path (netify-stats.chart, not view-namespaced)

--- htdocs/luci-static/resources/view/netify-stats/categories.js
+++ htdocs/luci-static/resources/view/netify-stats/categories.js
@@ -1,4 +1,4 @@
 'use strict';
-'require netify-stats.view.chart as chart';
+'require netify-stats.chart as chart';
 
 return chart.render('cats', _('Netify — Category Traffic'));
--- htdocs/luci-static/resources/view/netify-stats/overview.js
+++ htdocs/luci-static/resources/view/netify-stats/overview.js
@@ -1,4 +1,4 @@
 'use strict';
-'require netify-stats.view.chart as chart';
+'require netify-stats.chart as chart';
 
 return chart.render('apps', _('Netify — Application Traffic'));
